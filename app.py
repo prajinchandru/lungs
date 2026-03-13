1d24731b2c9ef79ca29e5512ba4edeb6f03aa01c
prajinchandru <prajinchandru2@gmail.com>
2026-03-13 21:10:46 +0530

    Replace TensorFlow model loading with Keras

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-import tensorflow as tf
 import numpy as np
 from PIL import Image
 import gdown
 import os
+from keras.models import load_model
 
 MODEL_ID = "1yL5KwOn8RTHq5ANhK6qSf_93ccxyGQeY"
 MODEL_PATH = "model.h5"
@@ -12,19 +12,19 @@ if not os.path.exists(MODEL_PATH):
     url = f"https://drive.google.com/uc?id={MODEL_ID}"
     gdown.download(url, MODEL_PATH, quiet=False)
 
-model = tf.keras.models.load_model(MODEL_PATH)
+model = load_model(MODEL_PATH)
 
 st.title("Lung Disease Detection")
 
-file = st.file_uploader("Upload X-ray Image", type=["jpg", "jpeg", "png"])
+file = st.file_uploader("Upload X-ray Image", type=["jpg","png","jpeg"])
 
 if file is not None:
     img = Image.open(file).convert("RGB")
-    st.image(img, use_column_width=True)
+    st.image(img)
 
-    img = img.resize((224, 224))
-    img = np.array(img) / 255.0
-    img = np.expand_dims(img, axis=0)
+    img = img.resize((224,224))
+    img = np.array(img)/255.0
+    img = np.expand_dims(img,axis=0)
 
     if st.button("Predict"):
         pred = model.predict(img)
